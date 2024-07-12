19553a5d4bf1db59b262df656a2b3b21b78337ee
Shahm Barmada <barmada.sha@gmail.com>
2024-07-12 14:03:10 +0300

    removed un-needed files for now

--- src/middleware/auth.ts
+++
@@ -1,7 +0,0 @@
-export default defineNuxtRouteMiddleware((to, from) => {
-  if (from.path === "/") {
-    if (localStorage.getItem("id") && localStorage.getItem("name")) {
-      return navigateTo("/dashboard");
-    }
-  }
-});
