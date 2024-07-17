f53f5890798b127c079ae1e2abbd9da5bc70c4c7
Shahm Barmada <barmada.sha@gmail.com>
2024-07-17 15:44:43 +0300

    updated configurations

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,17 @@
 // @ts-check
-import withNuxt from "./.nuxt/eslint.config.mjs";
+import withNuxt from './.nuxt/eslint.config.mjs'
+
 export default withNuxt().overrideRules({
-  "@typescript-eslint/no-explicit-any": "off",
-});
+  '@typescript-eslint/no-explicit-any': 'off',
+  'vue/max-attributes-per-line': [
+    'error',
+    {
+      singleline: {
+        max: 3,
+      },
+      multiline: {
+        max: 1,
+      },
+    },
+  ],
+})
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,18 +3,23 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   ssr: false,
   srcDir: 'src',
-  modules: ['@nuxt/eslint', '@nuxt/ui', "@nuxt/image"],
+  modules: ['@nuxt/eslint', '@nuxt/ui', '@nuxt/image'],
   ui: {
     icons: ['carbon'],
   },
   image: {
     localServer: {
-      baseURL: "http://localhost:3100/images"
-    }
+      baseURL: 'http://localhost:3100/images',
+    },
   },
   css: ['~/assets/globals.css'],
   app: {
     pageTransition: { name: 'page', mode: 'out-in' },
     layoutTransition: { name: 'layout', mode: 'out-in' },
+  },
+  eslint: {
+    config: {
+      stylistic: true,
+    },
   },
 })
