1b9edfbfd143a6a9da567e3066708ee1b527356a
Shahm Barmada <barmada.sha@gmail.com>
2024-08-01 07:20:00 +0300

    project configs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,6 +3,7 @@ import withNuxt from './.nuxt/eslint.config.mjs'
 
 export default withNuxt().overrideRules({
   '@typescript-eslint/no-explicit-any': 'off',
+  '@stylistic/max-len': ['error', { code: 120, tabWidth: 2 }],
   'vue/max-attributes-per-line': [
     'error',
     {
@@ -14,4 +15,9 @@ export default withNuxt().overrideRules({
       },
     },
   ],
+  '@stylistic/member-delimiter-style': [
+    'error',
+    { singleline: { delimiter: 'semi' } },
+  ],
+  'vue/singleline-html-element-content-newline': 'off',
 })
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -19,7 +19,7 @@ export default defineNuxtConfig({
   },
   eslint: {
     config: {
-      stylistic: true,
+      stylistic: false,
     },
   },
 })
